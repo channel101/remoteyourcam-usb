d7c397a9b814ade1d90694cb746fb142b0664da6
M <treeentertainment@outlook.kr>
2025-09-18 22:23:57 +0900

    Update NikonGetLiveViewImageCommand.java

--- app/src/main/java/com/remoteyourcam/usb/ptp/commands/nikon/NikonGetLiveViewImageCommand.java
+++ app/src/main/java/com/remoteyourcam/usb/ptp/commands/nikon/NikonGetLiveViewImageCommand.java
@@ -18,7 +18,7 @@ package com.remoteyourcam.usb.ptp.commands.nikon;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-import org.acra.reporter.ErrorReporter;
+import org.acra.ACRA;
 
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
@@ -116,7 +116,7 @@ public class NikonGetLiveViewImageCommand extends NikonCommand {
                 try {
                     haveAddedDumpToAcra = true;
                     String hex = PacketUtil.hexDumpToString(b.array(), start, length < 728 ? length : 728);
-                    ACRA.getErrorReporter().putCustomData("liveview hexdump", hex);
+                    ACRA.errorReporter.putCustomData("liveview hexdump", hex);
                 } catch (Throwable e) {
                     // no fail
                 }
